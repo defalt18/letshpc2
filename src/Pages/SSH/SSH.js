3cc877868f75235773c5c52ced85da5fe1eef8a5
Kushagra Pathak <45987083+defalt18@users.noreply.github.com>
2021-04-03 14:33:53 +0530

    Update SSH.js

--- src/Pages/SSH/SSH.js
+++ src/Pages/SSH/SSH.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./SSH.css"
 import Boxes from './Boxes'
-import Run_Boxes from './Run_boxes'
+import Run_Boxes from './Run_Boxes'
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import { IconButton } from '@material-ui/core';
 

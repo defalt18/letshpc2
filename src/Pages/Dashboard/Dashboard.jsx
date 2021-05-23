468ff284259c68f91442de80134f7082c4f79266
Kushagra Pathak <kushagrapathak@Kushagras-MacBook-Air.local>
2021-05-23 15:18:12 +0530

    Dashboard fixed

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import './Dashboard.css'
 import Chip from '@material-ui/core/Chip';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import Avatar from '@material-ui/core/Avatar';
+import plot_img from './sample_plot.png'
+import Button from '@material-ui/core/Button';
+import avatar_dummy from './avatar_yellow.png'
 
 function Dashboard() {
 
@@ -15,36 +19,101 @@ function Dashboard() {
                 <h3 style={{ color: `${page === 1 ? "white" : "lightgray"}` }} onClick={() => setpage(1)}>Report Generation</h3>
                 <h3 style={{ color: `${page === 2 ? "white" : "lightgray"}` }} onClick={() => setpage(2)}>Data Visualisation</h3>
                 <h3 style={{ color: `${page === 3 ? "white" : "lightgray"}` }} onClick={() => setpage(3)}>Profile</h3>
-                <h3><a href="/" style={{textDecoration:'none', color:"lightgray"}}>Logout</a></h3>
+                <h3><a href="/" style={{ textDecoration: 'none', color: "lightgray" }}>Logout</a></h3>
             </div>
             {
                 page === 0 ?
-                <div class="page">
-                    <h2>This is Tutorials Page</h2>
-                    <div class="tutorial__list">
-                    {
-                        Array(5).fill().map((_, i) => (
-                            <div class="tutorial__item">
-                                <div style={{display:'flex', gap:'10px', alignItems:'center'}}>
-                                <h3>Topic Name</h3>
-                                <Chip variant="outlined" size="small" label="Beginner" style={{ color: 'green', borderColor: 'green' }} />
-                                </div>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero autem aperiam et! Repellat minus, omnis earum maxime totam cupiditate sint exercitationem tempora ipsam mollitia id officia qui commodi quo maiores dolores aliquam, 
-                                    similique cum dolorum pariatur, suscipit quibusdam voluptate? Eaque ex nisi alias error earum explicabo nobis praesentium obcaecati consequuntur.</p>
+                    <div class="page">
+                        <h2>This is Tutorials Page</h2>
+                        <div class="tutorial__list">
+                            {
+                                Array(5).fill().map((_, i) => (
+                                    <div class="tutorial__item">
+                                        <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
+                                            <h3>Topic Name</h3>
+                                            <Chip variant="outlined" size="small" label="Beginner" style={{ color: 'green', borderColor: 'green' }} />
+                                            <p style={{marginLeft:'auto', color:'lightgreen', cursor:'pointer'}}>Mark as Read</p>
+                                        </div>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero autem aperiam et! Repellat minus, omnis earum maxime totam cupiditate sint exercitationem tempora ipsam mollitia id officia qui commodi quo maiores dolores aliquam,
+                                            similique cum dolorum pariatur, suscipit quibusdam voluptate? Eaque ex nisi alias error earum explicabo nobis praesentium obcaecati consequuntur.</p>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div> : <></>
+            }
+            {
+                page === 1 ?
+                    <div class="page">
+                        <h1>Welcome to Report Generation page</h1>
+                        <div class="tutorial__list">
+                            <div className="Report_Boxes">
+                                <h2>Evaluation Status</h2>
+                                <Chip variant="outlined" size="small" label="Pending" style={{ color: 'yellow', borderColor: 'yellow', background: 'rgba(255,255,0,0.1)' }} />
                             </div>
-                        ))
-                    }
+                            <div className="Report_Boxes box_report">
+                                <h2>Go to Report</h2>
+                                <Chip variant="outlined" size="small" label="Pending" style={{ color: 'yellow', borderColor: 'yellow', background: 'rgba(255,255,0,0.1)' }} />
+                            </div>
+                        </div>
+                    </div> : <></>
+            }
+            {
+                page === 2 ? <div class="page">
+                    <h1>Welcome to Data Visualisation page</h1>
+                    <div class="tutorial__list">
+                        <div className="Report_Boxes">
+                            <h2>Upload Data</h2>
+                            <Button style={{ background: 'rgb(0,150,255)', color: 'white' }}>
+                                Upload data as CSV
+                            </Button>
+                        </div>
                     </div>
+                    <h2>Recently Generated Plots</h2>
+                    <div class="plots__dashboard">
+                        {
+                            Array(5).fill().map((_, i) => (
+                                <div key={i} className="plot">
+                                    <img src={plot_img} alt="" />
+                                    <h2>Figure {i}</h2>
+                                    <p>Figure name goes here</p>
+                                </div>
+                            ))
+                        }
+                    </div>
+
                 </div> : <></>
             }
             {
-                page === 1 ? <div class="page">This is Report Generation Page</div> : <></>
-            }
-            {
-                page === 2 ? <div class="page">This is Data Visualisation Page</div> : <></>
-            }
-            {
-                page === 3 ? <div class="page">This is Data Profile Page</div> : <></>
+                page === 3 ? <div class="page">
+                    <h1>User Profile</h1>
+                    <div className="tutorial__list">
+                        <div className="profile__item">
+                            <div class="desc__profile">
+                                <div className="info">
+                                    <h3>Name :</h3>
+                                    <p>John Doe</p>
+                                    <Chip variant="outlined" size="small" label="Student" style={{ color: 'rgb(0,150,255)', borderColor: 'rgb(0,150,255)' }} />
+                                </div>
+                                <div className="info">
+                                    <h3>Email :</h3>
+                                    <p>JohnDoe@gmail.com</p>
+                                </div>
+                                <div className="info">
+                                    <h3>Student ID :</h3>
+                                    <p>201xxxxxx</p>
+                                </div>
+                                <div className="info">
+                                    <p><b>Bio : </b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, aspernatur repellat quidem animi numquam eaque aliquam iusto architecto deserunt! Asperiores, ipsam necessitatibus corrupti sunt sequi quis quaerat, libero eius exercitationem error accusamus aspernatur laudantium adipisci 
+                                        unde doloribus nemo nobis odit tempore iusto, ducimus tenetur? Ratione molestiae voluptates molestias dolore esse.</p>
+                                </div>
+                            </div>
+                            <div class="profile__right">
+                            <Avatar src={avatar_dummy} alt="K" style={{height:200,width:200}}/>
+                            </div>
+                        </div>
+                    </div>
+                </div> : <></>
             }
         </div>
     )

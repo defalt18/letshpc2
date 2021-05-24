403dad981aed1a750b34289591b3776fd596248e
Kushagra Pathak <kushagrapathak@Kushagras-MacBook-Air.local>
2021-05-24 11:37:28 +0530

    Added Animations

--- src/Pages/Dashboard/Report.jsx
+++ src/Pages/Dashboard/Report.jsx
@@ -1,22 +1,23 @@
 import { Close } from '@material-ui/icons'
 import React from 'react'
+import SaveIcon from '@material-ui/icons/Save';
 import plot_img from './sample_plot.png'
+import { IconButton } from '@material-ui/core'
 import './Report.css'
 
 
 function Report({func}) {
+
+    const [Class, setClass] = React.useState("report__page");
+
     return (
-        <div class="report__page">
-            <Close onClick={()=>{
-                func(false);
-            }} style={{
-                position: 'fixed',
-                top: 10,
-                right: 10,
-                fontSize: 50,
-                color: 'gray',
-                cursor: 'pointer',
-            }} />
+        <div class={Class}>
+            <IconButton id="close_icon" onClick={()=>{
+                setClass("report__page__down")
+                setTimeout(() => {func(false);}, 500);
+            }} >
+                <SaveIcon />
+            </IconButton>
             <div class="recent__plots">
                 <h2 className="header__side">Generated Plots</h2>
                 {
